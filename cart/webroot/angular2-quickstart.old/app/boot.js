92e2b727bd95fc22bdce87467959def84ab39b15
nsarafa <nicksarafa@gmail.com>
2016-07-10 04:45:13 -0400

    fixup ~ remove quickstart angular app

--- cart/webroot/angular2-quickstart.old/app/boot.js
+++
@@ -1,6 +0,0 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent, [ng.http.HTTP_PROVIDERS]);
-  });
-})(window.app || (window.app = {}));
-
